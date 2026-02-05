87030cc0a18b3de1fe4f311efb291445aad9d8be
BryanClei <bryanBclerigo@gmail.com>
2026-02-05 22:54:21 +0800

    Home Commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import Button from "./Button.jsx";
 import { Hello, HelloWithoutJSX } from "./Hello.jsx";
 import { UserProfile } from "./UserProfile.jsx";
 import { ContactForm } from "./ContactForm.jsx";
+import { StyledForm } from "./StyledForm.jsx";
+import { CandidateProfile } from "./CandidateProfile.jsx";
 import "./App.css";
 
 function App() {
   return (
     <div>
+      <CandidateProfile />
+      <StyledForm />
       <ContactForm />
       <UserProfile />
       <Hello />
--- src/StyledForm.jsx
+++ src/StyledForm.jsx
@@ -1,11 +1,11 @@
 export const StyledForm = () => {
   return (
     <form action="">
-      <label for="username">Username: </label>
-      <input type="text" id="username" class="form-input" />
+      <label htmlFor="username">Username: </label>
+      <input type="text" id="username" className="form-input" />
       <br />
-      <label for="email">Email: </label>
-      <input type="email" id="email" class="form-input" tabindex="1" />
+      <label htmlFor="email">Email: </label>
+      <input type="email" id="email" className="form-input" tabIndex="1" />
     </form>
   );
 };
